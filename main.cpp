c3946a6b4690c901ed0b6fff7119562dd40ea1d9
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2023-10-11 17:53:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,23 @@ int main() {
 
 	Polynomial p4 = p1.derivative();
 	cout << "p4 = p1': "; p4.print(); cout << endl;
+	cout << "Многочлен p1 имеет степень: " << p1.getDegree() << endl;
+	cout << "Многочлен p4 имеет степень: " << p4.getDegree() << endl << endl;
 	cout << "p1: "; p1.print(); cout << endl;
+	Polynomial p5 = p1 / p4;
+	cout << "p5: "; p5.print(); cout << endl;
+
+	cout << endl;
+	cout << "p1: "; p1.print(); cout << ". ";
+	cout << "Многочлен p1 имеет степень: " << p1.getDegree() << endl;
+	cout << "p2: "; p2.print(); cout << ". ";
+	cout << "Многочлен p2 имеет степень: " << p2.getDegree() << endl;
+	cout << "p3: "; p3.print(); cout << ". ";
+	cout << "Многочлен p3 имеет степень: " << p3.getDegree() << endl;
+	cout << "p4: "; p4.print(); cout << ". ";
+	cout << "Многочлен p4 имеет степень: " << p4.getDegree() << endl;
+	cout << "p5: "; p5.print(); cout << ". ";
+	cout << "Многочлен p5 имеет степень: " << p5.getDegree() << endl;
+
 	return 0;
 }
