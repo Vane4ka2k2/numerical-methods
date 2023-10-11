94f5bb3cf128f7e4d011b392503cdf9094bf094a
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2023-10-11 04:15:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
 #include "Polynomial.h"
+#include <iostream>
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 
-	Polynomial p1;
-	Polynomial p2;
+	Polynomial p1({ 1, -1, 1, 0, 2, 3 });
+	Polynomial p2({ 0, 1, 2, 1 });
 
-	p1.input();
-	p2.input();
-
+	cout << "p1: "; p1.print(); cout << ". ";
+	cout << "Многочлен p1 имеет степень: " << p1.getDegree() << endl;
+	cout << "p2: "; p2.print(); cout << ". ";
+	cout << "Многочлен p2 имеет степень: " << p2.getDegree() << endl;
 	Polynomial p3 = p1 / p2;
+	cout << "p3 = p1 / p2: "; p3.print(); cout << ". ";
+	cout << "Многочлен p3 имеет степень: " << p3.getDegree() << endl;
 
-	cout << "p3: " << p3 << endl;
-
-	system("pause");
+	return 0;
 }
