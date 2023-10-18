835b50f05bf144c286777b407828664f332e8b14
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2023-10-18 19:38:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Polynomial.h"
+#include "Polynomial.hpp"
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -6,29 +6,27 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    Polynomial p1({ 2, 3, 3, 1 });
-    Polynomial p2({ 1, 2, 2, 1 });
-    Polynomial p3 = p1.gcd(p2);
-    cout << "p1: " << p1 << endl;
-    cout << "p2: " << p2 << endl;
-    cout << "НОД p1 и p2: " << p3 << endl;
+    Polynomial p({ -3, 1, 1, 0, 1 });
+    Polynomial q({ -3, 1, 1, 1 });
+    cout << "P: " << p << endl;
+    cout << "Q: " << q << endl;
+    cout << "НОД(P, Q): " << p.gcd(q) << endl;
+
     cout << endl;
-    Polynomial p4({ 0, 1, 0, -2, 0, 1 });
-    Polynomial p5 = p4.derivative();
-    Polynomial p6 = p4.gcd(p5);
-    cout << "p4: " << p4 << endl;
-    cout << "p4': " << p5 << endl;
-    cout << "НОД p4 и p4': " << p6 << endl;
+    Polynomial p1({ 0, 1, 0, -2, 0, 1 });
+    cout << "p1: " << p1 << endl;
+    cout << "p1': " << p1.derivative() << endl;
+    cout << "Многочлен без кратных корней: " << p1.removeMultiplicity() << endl;
 
     return 0;
 }
 
 /* output
-p1: x ^ 3 + 3x ^ 2 + 3x + 2
-p2 : x ^ 3 + 2x ^ 2 + 2x + 1
-НОД p1 и p2 : x ^ 2 + x + 1
+P: x^4 + x^2 + x - 3
+Q: x^3 + x^2 + x - 3
+НОД(P, Q): 27x - 27
 
-p4 : x ^ 5 - 2x ^ 3 + x
-p4': 5x^4 - 6x^2 + 1
-НОД p4 и p4': -x^2 + 1
+p1: x^5 - 2x^3 + x
+p1': 5x^4 - 6x^2 + 1
+Многочлен без кратных корней: -x^3 + x
 */
