29ceb5a7c175490c7f1e7b7d1fdc5a659f6821d8
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2023-10-18 07:46:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,28 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    Polynomial p1({ 0, 1, 0, -2, 0, 1 });
-    Polynomial der_p1 = p1.derivative();
-    Polynomial p2 = p1.gcd(der_p1);
-    Polynomial p3 = p1.removeMultiplicity();
-
+    Polynomial p1({ 2, 3, 3, 1 });
+    Polynomial p2({ 1, 2, 2, 1 });
+    Polynomial p3 = p1.gcd(p2);
     cout << "p1: " << p1 << endl;
-    cout << "p1': " << der_p1 << endl;
-    cout << "НОД p1 и p1': " << p2 << endl;
-    cout << "p1 без кратных корней: " << p3 << endl;
+    cout << "p2: " << p2 << endl;
+    cout << "НОД p1 и p2: " << p3 << endl;
+    cout << endl;
+    Polynomial p4({ 0, 1, 0, -2, 0, 1 });
+    Polynomial p5 = p4.derivative();
+    Polynomial p6 = p4.gcd(p5);
+    cout << "p4: " << p4 << endl;
+    cout << "p4': " << p5 << endl;
+    cout << "НОД p4 и p4': " << p6 << endl;
 
     return 0;
 }
+
+//output
+/*p1: x ^ 3 + 3x ^ 2 + 3x + 2
+p2 : x ^ 3 + 2x ^ 2 + 2x + 1
+НОД p1 и p2 : x ^ 2 + x + 1
+
+p4 : x ^ 5 - 2x ^ 3 + x
+p4': 5x^4 - 6x^2 + 1
+НОД p4 и p4': -x^2 + 1*/
