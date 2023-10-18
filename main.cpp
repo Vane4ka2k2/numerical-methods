29cad7fc2181eb59190688961d1ad7594c6c0a74
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2023-10-18 04:29:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,20 +7,14 @@ int main() {
     setlocale(LC_ALL, "Russian");
 
     Polynomial poly1({ 0, 1, 0, -2, 0, 1 });
-    Polynomial poly2({ 1, 0, -6, 0, 5 });
+    Polynomial poly2 = poly1.derivative();
     Polynomial poly3 = poly1.gcd(poly2);
-    Polynomial poly4 = poly1 / poly3;
-    Polynomial poly5 = poly2 / poly3;
-    Polynomial poly6 = poly1 % poly3;
-    Polynomial poly7 = poly2 % poly3;
+    Polynomial poly4 = poly1.removeMultiplicity();
 
     cout << "poly1: " << poly1 << endl;
-    cout << "poly2: "; poly2.print(); cout << endl;
-    cout << "poly3: "; poly3.print(); cout << endl;
-    cout << "poly4: "; poly4.print(); cout << endl;
-    cout << "poly5: "; poly5.print(); cout << endl;
-    cout << "poly6: "; poly6.print(); cout << endl;
-    cout << "poly7: " << poly7 << endl;
+    cout << "poly2: " << poly2 << endl;
+    cout << "poly3: " << poly3 << endl;
+    cout << "poly4: " << poly4 << endl;
 
     return 0;
 }
