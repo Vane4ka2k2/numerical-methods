e99c3238a36cd7a9372c1fd9ec9bf1d5e9122022
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2023-10-18 04:41:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Polynomial.hpp"
+#include "Polynomial.h"
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -7,13 +7,13 @@ int main() {
     setlocale(LC_ALL, "Russian");
 
     Polynomial p1({ 0, 1, 0, -2, 0, 1 });
-    Polynomial derp1 = p1.derivative();
-    Polynomial p2 = p1.gcd(derp1);
+    Polynomial der_p1 = p1.derivative();
+    Polynomial p2 = p1.gcd(der_p1);
     Polynomial p3 = p1.removeMultiplicity();
 
     cout << "p1: " << p1 << endl;
-    cout << "p1': " << derp1 << endl;
-    cout << "нод p1 и p1': " << p2 << endl;
+    cout << "p1': " << der_p1 << endl;
+    cout << "НОД p1 и p1': " << p2 << endl;
     cout << "p1 без кратных корней: " << p3 << endl;
 
     return 0;
