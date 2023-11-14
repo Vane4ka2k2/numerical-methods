e709d170f759e8a1434f4ec946090b97e07c071b
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2023-11-14 18:42:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,19 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    Polynomial p1({ -3, 4, 0, 1 });
-    double result = p1.methodNewton();
+    /*Polynomial p1({ -3, 4, 0, 1 });
+    double result1 = p1.methodNewton(0, 1);
 
     cout << "p: " << p1 << endl;
-    cout << "Корень: " << result << endl;
+    cout << "Корень: " << result1 << endl;*/
+
+    Polynomial p2({ 1, -24, 3, 1 });
+    double result1 = p2.methodChordsAndTangents(-7, -6);
+    double result2 = p2.methodChordsAndTangents(3, 4);
+
+    cout << "p2: " << p2 << endl;
+    cout << "Корень: " << result1 << endl;
+    cout << "Корень: " << result2 << endl;
 
     return 0;
 }
